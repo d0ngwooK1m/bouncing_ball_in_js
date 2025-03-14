3acf5e25b1d50272818feb2512ab42ef0e65e87a
d0ngwooK1m <76802735+d0ngwooK1m@users.noreply.github.com>
2025-03-14 16:41:51 +0900

    ball.js 1차 구현 완료

--- ball.js
+++ ball.js
@@ -1,3 +1,41 @@
-class Ball {
-    
+export class Ball {
+    constructor(stageWidth, stageHeight, radius, speed) {
+        this.radius = radius;
+        this.vx = speed;
+        this.vy = speed;
+
+        const diameter = radius * 2;
+        this.x = radius + (Math.random() * (stageWidth - diameter));
+        this.y = radius + (Math.random() * (stageHeight - diameter));
+
+
+    }
+
+    draw(ctx, stageWidth, stageHeight) {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        this.bounceWindow(stageWidth, stageHeight);
+
+        ctx.fillStyle = "#fdd700";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    bounceWindow(stageWidth, stageHeight) {
+        const minX = this.radius;
+        const maxX = stageWidth - this.radius;
+        const minY = this.radius;
+        const maxY = stageHeight - this.radius;
+
+        if (this.x <= minX || this.x >= maxX) {
+            this.vx *= -1;
+            this.x += vx;
+        }
+        if (this.y <= minY || this.y >= maxY) {
+            this.vy *= -1;
+            this.y += vy;
+        }
+    }
 }
